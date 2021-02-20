6e34d692daa408087baed7c4d8b887825e1e4c76
Johnny <jaoldaker1@gmail.com>
2021-02-19 20:04:42 -0500

    Front end started

--- MTG_Database/mtg_app/views.py
+++ MTG_Database/mtg_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from .models import *
+import pickle
+
+keys = pickle.load(open('mtg_app/key_pickle.p','rb'))
+
 def index(request):
-	return render(request,'index.html')
+	context = {
+				'keywords':Keywords.objects.all(),
+				'rarities': Rarity.objects.all(),
+				'types': Type_line.objects.all(),
+				
+				}
+
+	return render(request,'index.html', context)
